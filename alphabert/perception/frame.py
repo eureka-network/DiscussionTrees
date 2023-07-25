1e559c0866ff263b1a0780e30f75d0219d50ab03
jorgeantonio21 <matroid@outlook.com>
2023-07-25 15:13:24 +0100

    :bricks: :bricks: brick by brick first commit

--- alphabert/perception/frame.py
+++ alphabert/perception/frame.py
@@ -1,8 +1,12 @@
+from .neo4j_reader import Neo4jReader
+
+
 class Frame:
-    def __init__(self, height: int, level: int, group: str):
+    def __init__(self, height: int, level: int, group: str, reader: Neo4jReader):
         self.height = height
         self.level = level
         self.group = group
+        self._reader = reader
 
     def execute_input(self, input: str):
         print("I love Rust !")
