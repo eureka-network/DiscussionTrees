08964d302db3316bce1e14460813d6c195cd9231
benjaminbollen <ben@doru.io>
2023-08-31 21:44:11 +0100

    remove commented out deduplication of sha256

--- discussion_trees/ingest/ingester.py
+++ discussion_trees/ingest/ingester.py
@@ -1,35 +1,10 @@
 import os
 import cbor2
-# import hashlib
 
 from discussion_trees.graph_store import Graph
 from discussion_trees.document_store import Document, UnitData
 
 from discussion_trees.hasher import calculate_sha256_from_file, calculate_sha256
-
-# todo: remove once confirmed
-# def calculate_sha256_from_file(file_path):
-#     """Calculate the SHA256 hash of a file."""
-#     sha256 = hashlib.sha256()
-#     with open(file_path, 'rb') as file:
-#         # Reading in chunks to handle large files
-#         for chunk in iter(lambda: file.read(4096), b''):
-#             sha256.update(chunk)
-#     return sha256.hexdigest()
-
-
-# def calculate_sha256(data):
-#     """Calculate the SHA256 hash of a string or bytes."""
-#     sha256 = hashlib.sha256()
-    
-#     if isinstance(data, str):
-#         sha256.update(data.encode('utf-8'))
-#     elif isinstance(data, bytes):
-#         sha256.update(data)
-#     else:
-#         raise TypeError("Expected data to be of type str or bytes")
-    
-#     return sha256.hexdigest()
 
 
 def filepath_to_document_name(filepath):
